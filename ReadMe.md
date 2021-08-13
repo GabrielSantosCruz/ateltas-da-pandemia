567595cbe5510b2d30e71e9bd9f05590cc58a7b2
Gabriel Santos <82888506+GabrielSantosCruz@users.noreply.github.com>
2021-08-13 07:29:40 -0300

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# Atletas de quarentena
+
+## Problema
+
+"A disciplina EXA854 - MI - ALGORITMOS do primeiro semestre do curso de Engenharia de Computação da Universidade Estadual de Feira de Santana (UEFS), que trabalha com o método de ensino Aprendizagem Baseada em Problemas (PBL), visa aproximar o estudante da área  de computação com problemas cotidianos com o fito de estimular o aluno a buscar conhecimento. Dito isso, a pofessora da disciplina resolveu passar o seguite problema com o objetivo de testar nossos conhecimentos e utiliza-los para resolver um problema cotidiano."
+
+## Ao final da leitura dos dados, o sistema “UEFS pelos Atletas” deverá exibir um relatório com as seguintes informações:
+
+| Nº | Descrição | 
+| :----------: | --------- | 
+| 01 | Quantidade de atletas monitorados | 
+| 02 | A quantidade e a porcentagem de atletas que apresentaram sintomas; |
+| 03 | Idade média de todos os atletas, dos atletas sem sintomas, e dos atletas sintomáticos; |
+| 04 | A temperatura corporal mais alta relatada; |
+| 05 | Dentre os que apresentaram sintomas, a idade do atleta mais novo e do atleta mais velho;|
+| 06 | Um recorte por gênero dos atletas que tomaram o “kit COVID”, indicando ainda, dentre estes, a quantidade de homens e mulheres que tiveram ou não sintomas;|
+| 07 | Um recorte por gênero (M/F) e por sintomas (S/N) dos atletas que trouxeram medalhas para casa, especificando a quantidade de medalhas de ouro, prata e bronze. |
---
+++ main.py
@@ -0,0 +1,49 @@
+#/*******************************************************************************
+#Autor: Gabriel Santos Cruz
+#Componente Curricular: Algoritmos I
+#Concluido em: xx/xx/2021
+#Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+#trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+#apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+#de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+#do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+#******************************************************************************************/
+
+# Entrada de dados:
+print("Sistema UEFS pelos atletas\n")
+
+rep = 's'
+
+# Se eu digitar um numero em vez de uma string e dar erro?
+
+while rep.lower() == 's':
+
+    age = int(input("Digite sua idade: "))
+    gender = input("Digite seu sexo (M/F): ")
+    fever = input("Teve febre (S/N): ")
+
+    if fever.lower() == 's':
+        temperatura = float(input("Qual foi a maior temperatura detectada"))
+
+    else:
+        symptom = input("Teve algum sintoma (S/N): ")
+
+    kit = input("Tomou o kit Covid ao retornar ao Brasil (S/N): ")
+
+    medal = input("Ganhou alguma medalha? (S/N): ")
+    
+# Calcular a quantidade de medalhas e qual o seu tipo (Ouro, Prata, Bronze)
+
+    if medal.lower() == 's':
+
+        gold = int(input("Quantas medalhas de ouro?: "))
+        silver = int(input("Quantas medalhas de prata?: "))
+        bronze = int(input("Quantas medalhas de bronze?: "))
+    
+    rep = input("Deseja cadastrar um novo atleta?(S/N): ")
+
+    if rep.lower() == 'n':
+
+        print("O cadastramento foi encerrado!!!")
+
+# Processamento de dados:
